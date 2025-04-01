ae247d93175de6641a46603e27b5944e39d53aa1
seishisw <seishisw@gmail.com>
2025-04-01 10:22:25 -0400

    update:

---
+++ JAVASCRIPTDISCOVERY/index.md
@@ -0,0 +1,46 @@
+USING VARIABLES:
+
+let x,y,z:
+
+
+ALL JAVASCRIPTS WILL GO INSIDE THE:
+
+
+<script>
+let x = 5;
+y = 6;
+z = x + y;
+</script>
+
+
+
+.innerHTML == gets the element contained within the HTML
+for variabel insertion 
+
+
+
+
+let
+var 
+const
+float
+
+
+refereecning functions
+<button onClick="sumValues(7,8)">
+sumValues=fucntion 
+
+
+creating custom fucntions: 
+
+function (NAME OF FUNCTION) (PARAM1, PARAM2);
+
+
+
+BUILT IN JAVASCRIPT FUNCTIONS
+
+mouseenter
+mouseleave
+mouseover
+mouseout
+click
---
+++ JAVASCRIPTDISCOVERY/new.js
@@ -0,0 +1,58 @@
+let number=10;
+let nah=false;
+let name = "matt";
+let age = 20;
+let coding =true;
+speed=2;
+speed2=1;
+positionX=0;
+positionY=0;
+
+document.getElementById("practice").innerHTML = "HELLO, my name is" + name + " I am " + age + " years old"
+
+// var myVariable = "Hello, world!";
+// document.getElementById("output").textContent = myVariable;
+
+const button = document.getElementById("alertButton");
+button.addEventListener('click',function(){
+    alert("BUTTON CLICKED");
+});
+
+
+const annimationbutton = document.getElementById("annimationbutton");
+const bluedot = document.getElementById("bluedot");
+
+// annimationbutton.addEventListener('mouseenter',function(){
+//     bluedot.classList.add('annimate');
+// });
+
+
+
+// element.addEventListener('mouseleave', function() {
+//     bluedot.classList.remove('annimate2');
+//   });
+
+
+
+annimationbutton.addEventListener('mouseover',function(){
+    for(let i=0;i<window.innerWidth;i++){
+        positionX+=speed;
+        positionY+=speed;
+        bluedot.style.left = positionX +'px';
+        bluedot.style.top = positionY +'px';
+    }
+});
+
+annimationbutton.addEventListener('mouseout',function(){
+
+        for(let i=0;i<window.innerWidth;i++){
+        positionX-=speed;
+        if(positionY>=200){
+            positionY-=speed;
+        }else{
+            positionY=positionY;
+        }
+        bluedot.style.left = positionX +'px';
+        bluedot.style.top = positionY +'px';
+    }
+});
